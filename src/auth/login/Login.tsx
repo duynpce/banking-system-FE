b05ac6d0ee06612ecc86915df89b744746807162
duynpce <a23.thvl03@gmail.com>
2026-02-25 22:34:46 +0700

    feat : remember username  and add notification when login failed

--- src/auth/login/Login.tsx
+++ src/auth/login/Login.tsx
@@ -1,14 +1,45 @@
-import { useState } from "react";
-import { Link } from "react-router-dom";
+import {  useEffect, useRef } from "react";
+import { Link, useSearchParams } from "react-router-dom";
 import { ROOT_API_URL } from "../../shared/Constant";
 
 const Login = () => {
-  //implement set message when login failed later
-  const [message, setMessage] = useState<string>('');
+  const usernameRef = useRef<HTMLInputElement>(null);
+  const [searchParams] = useSearchParams();
+
+  useEffect(() => {
+    const savedUsername = sessionStorage.getItem("username");
+    if (savedUsername && usernameRef.current) {
+      usernameRef.current.value = savedUsername;
+    }
+  }, []);
+
+const getErrorMessage = (error: string | null): string => {
+    if (!error) return "";
+    if (error === "invalid-credentials") {
+      return "Not existed account or incorrect password";
+    } else if (error === "authentication-failed") {
+      return "Authentication failed";
+    } else {
+      return "unknown error";
+    }
+  };
+
+  const message = getErrorMessage(searchParams.get("error"));
+
+  const handleSubmit = (e: React.SubmitEvent<HTMLFormElement>) => {
+    const formData = new FormData(e.currentTarget);
+    const username = formData.get("username") as string;
+    
+    if (username) {
+      sessionStorage.setItem("username", username);
+    }
+  };
+
+  
+
   return (
-    <form id="login-form" action={`${ROOT_API_URL}/login`} method="POST"  >
-      <input type="text" name="username" placeholder="username"
-      />
+    <form id="login-form" action={`${ROOT_API_URL}/login`} method="POST" onSubmit={handleSubmit}>
+      <input type="text" name="username" placeholder="username" ref={usernameRef}/>
       <input type="password" name="password" placeholder="password" />
       {message && <p style={{ color: "red" }}>{message}</p>}
       <button type="submit"> Login </button>
@@ -19,3 +50,4 @@ const Login = () => {
 };
 
 export default Login;
+
